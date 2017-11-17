56abd91b5e78f051d5b44427a1766b3a40b0d7d3
Marc DellaVolpe <marc.dellavolpe@gmail.com>
2017-11-16 21:24:22 -0500

    use writer thread and queue

--- irclogger/channel.py
+++ irclogger/channel.py
@@ -13,11 +13,21 @@ class Channel(object):
         os.mkfifo(self.path)
 
     def remove(self):
-        if os.path.exists(self.path):
+        try:
             os.remove(self.path)
+        except OSError:
+            pass
 
-    def open(self, mode="r"):
-        return open(self.path, mode=mode)
+    def open(self, mode='r', blocking=True):
+        flag = 0
+        if mode == 'r':
+            flag |= os.O_RDONLY
+        elif mode == 'w':
+            flag |= os.O_WRONLY
+        if not blocking:
+            flag |= os.O_NONBLOCK
+        fd = os.open(self.path, flag)
+        return os.fdopen(fd, mode)
 
     def write(self, line):
         with self.open("w") as f:
--- irclogger/handler.py
+++ irclogger/handler.py
@@ -1,12 +1,38 @@
+import time
+import threading
 from logging import Handler
+from Queue import Queue, Full
+
+from .channel import Channel
+
+
+def ensure_written(channel, msg):
+    while True:
+        try:
+            return channel.write(msg)
+        except OSError:
+            time.sleep(1)
+
+
+def channel_writer(channel, queue):
+    while True:
+        msg = queue.get()
+        ensure_written(channel, msg)
 
 
 class IRCHandler(Handler):
-
-    def __init__(self, channel, *args, **kwargs):
-        super(IRCHandler, self).__init__(*args, **kwargs)
+    def __init__(self, channel, max_size=0, **kwargs):
+        super(IRCHandler, self).__init__(**kwargs)
+        if isinstance(channel, basestring):
+            channel = Channel(channel)
         self.channel = channel
+        self.queue = Queue(maxsize=max_size)
+
+        self.thread = threading.Thread(target=channel_writer, args=(self.channel, self.queue))
+        self.thread.start()
 
     def emit(self, record):
-        self.channel.write(self.format(record))
-
+        try:
+            self.queue.put_nowait(self.format(record))
+        except Full:
+            pass
